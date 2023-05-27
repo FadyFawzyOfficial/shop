f35bb0b74ebb2dd486de643c028f369ec5ddfb67
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-05-27 15:55:09 +0300

    9. Use Providers & Listeners Approach

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../models/product.dart';
+import '../providers/products.dart';
 import 'product_gird_item.dart';
 
 class ProductsGrid extends StatelessWidget {
-  final List<Product> products;
-
-  const ProductsGrid({super.key, required this.products});
+  const ProductsGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.products;
     return GridView.builder(
       padding: const EdgeInsets.all(8),
       itemCount: products.length,
