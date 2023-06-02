9ee6aec77c9bdd0f7f9b6af3aa904150492bbb1b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-02 19:28:51 +0300

    2. Set Listener (Change Focus Listener) to update ImagePreview when ImageInput lose its focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,8 +11,16 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   //! For the Image Preview I want to get access the url text of TextFormField
-  //! to update the Image Preview with the that ImageURL text.
+  //! and it's FocusNode to update the Image Preview with that ImageURL text and
+  //! while the ImageUrl Input Field loosing the FocusNode.
   final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +70,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
                     textInputAction: TextInputAction.done,
                     keyboardType: TextInputType.url,
                     controller: _imageUrlController,
+                    //* Review "Image Input & Image Preview.mp4" video if you
+                    //* get Stacked
+                    //! With this _imageUrlFocusNode and its added Listener,
+                    //! we get the preview when we lose focus
+                    focusNode: _imageUrlFocusNode,
                     //! onFieldSubmitted Dismisses the SoftKeyboard when done
                     //! onEditingComplete doesn't
                     onFieldSubmitted: (_) => setState(() {}),
@@ -75,10 +88,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
     );
   }
 
-  //! Don't forget to dispose the Controllers.
+  //! Don't forget to dispose the Controllers & Listeners.
   @override
   void dispose() {
     _imageUrlController.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _imageUrlFocusNode.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 }
