3b4462820983e294c635ec469d85e7e39497c1e7
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-02 19:22:35 +0300

    1. Add Image Input & Image Preview to EditProductScreen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,10 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  //! For the Image Preview I want to get access the url text of TextFormField
+  //! to update the Image Preview with the that ImageURL text.
+  final _imageUrlController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +39,46 @@ class _EditProductScreenState extends State<EditProductScreen> {
               maxLines: 3,
               // textInputAction: TextInputAction.next,
             ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: const EdgeInsetsDirectional.only(top: 16, end: 16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: _imageUrlController.text.isEmpty
+                      ? const Text('Enter a URL')
+                      : Image.network(
+                          _imageUrlController.text,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: 'Image URL'),
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.url,
+                    controller: _imageUrlController,
+                    //! onFieldSubmitted Dismisses the SoftKeyboard when done
+                    //! onEditingComplete doesn't
+                    onFieldSubmitted: (_) => setState(() {}),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+
+  //! Don't forget to dispose the Controllers.
+  @override
+  void dispose() {
+    _imageUrlController.dispose();
+    super.dispose();
   }
 }
