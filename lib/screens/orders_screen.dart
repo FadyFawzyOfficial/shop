0eb72fc3c2c8e6f9b8664cd83c5e5f01ef34c04e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-07 20:52:55 +0300

    2. Improve to Deal with unrelated Changes avoiding Refetching while Rebuilding

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,18 +5,47 @@ import '../providers/orders.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/order_list_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = 'orders';
 
   const OrdersScreen({super.key});
 
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+// 1. Convert to StatefulWidget.
+class _OrdersScreenState extends State<OrdersScreen> {
+  // 2. Add a property (Variable) of type Future.
+  late Future _orderFuture;
+
+  // 3. Add new method which return a Future.
+  // 4. In this method return the result of this call to fetch and set orders.
+  Future _obtainOrderFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchOrders();
+  }
+
+  // 5. You should add initState, set the result of method to the earlier variable.
+  @override
+  void initState() {
+    super.initState();
+    _orderFuture = _obtainOrderFuture();
+  }
+
+  // If you had something (some other state you're managing or anything like that)
+  // in this widget which cause build method to run again (rebuild),
+  // then fetchOrder() would of course, also be executed.
+  // We might want to avoid if just something else changed in this widget,
+  // if there is no reason to fetch new orders.
+  // You don't want to fetch new orders just because something else changed in
+  // this widget, which doesn't affect the orders.
   @override
   Widget build(BuildContext context) {
     // This print statement to be sure the build method should now build one time.
     print('building orders');
     //! Don't set up the listener here for Orders data when U use the FutureBuilder
     //! Because this will continue rebuilding the entire Screen.
-    final ordersProvider = Provider.of<Orders>(context, listen: false);
+    // final ordersProvider = Provider.of<Orders>(context, listen: false);
     return Scaffold(
       appBar: AppBar(title: const Text('Orders')),
       drawer: const AppDrawer(),
@@ -25,7 +54,8 @@ class OrdersScreen extends StatelessWidget {
       // And it takes a builder which will get the current snapshot to current state
       // of your future so that you can build different content based on what your future return.
       body: FutureBuilder(
-        future: ordersProvider.fetchOrders(),
+        // 6. Set future using variable initialized in initState()
+        future: _orderFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
