f6e7f538a953f4181625e5090f32848bce97baf6
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-06-16 16:41:31 +0300

    5. Refactor & Reduce Code of SingUp & SignIn Operations

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,22 +4,23 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
 class Auth with ChangeNotifier {
-  static const apiKey = 'AIzaSyCCZrB4cUxYue1WNE7s8xjrgn0j32rogmk';
-  static const signUpUrl =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
-  static const signInUrl =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+  static const apiKey = '?key=AIzaSyCCZrB4cUxYue1WNE7s8xjrgn0j32rogmk';
+  static const signUpUrlSegment = 'signUp';
+  static const signInUrlSegment = 'signInWithPassword';
+  static const authenticateUrl =
+      'https://identitytoolkit.googleapis.com/v1/accounts:';
 
   late String _token;
   late DateTime _expiryDate;
   late String _userId;
 
-  Future<void> signUp({
+  Future<void> _authenticate({
     required String email,
     required String password,
+    required String urlSegment,
   }) async {
     final response = await post(
-      Uri.parse('$signUpUrl$apiKey'),
+      Uri.parse('$authenticateUrl$urlSegment$apiKey'),
       body: json.encode({
         'email': email,
         'password': password,
@@ -29,20 +30,30 @@ class Auth with ChangeNotifier {
 
     print(response.body);
   }
+
+  // We return this future because this is the future which actually wraps our
+  // HTTP request and waits for it to complete.
+  // So, in order to have our loading spinner work correctly, we want to return
+  // the future which actually does the work.
+  // Without return, we would also return a future but this wouldn't wait for
+  // the future of authenticate to do its job.
+  Future<void> signUp({
+    required String email,
+    required String password,
+  }) async =>
+      _authenticate(
+        email: email,
+        password: password,
+        urlSegment: signUpUrlSegment,
+      );
 
   Future<void> signIn({
     required String email,
     required String password,
-  }) async {
-    final response = await post(
-      Uri.parse('$signInUrl$apiKey'),
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
-
-    print(response.body);
-  }
+  }) async =>
+      _authenticate(
+        email: email,
+        password: password,
+        urlSegment: signInUrlSegment,
+      );
 }
